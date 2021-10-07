1cd19d6415d9a2e8ce038121bfaa7c2ed2c8de6d
zeus-ctrl <noah.constable@gmail.com>
2021-10-07 11:30:13 -0500

    Re-do input/output.
    Co-authored-by: Cole Maxwell <cole-maxwell1@users.noreply.github.com>

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -38,41 +38,44 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
      * copy the non-vowels to the output buffer,
      * and use fwrite to write that out.
      */
-    __FILE__ userInput = fopen(userString)
+    char buffer[BUF_SIZE];
+    switch(argc) {
+        case 1:
+            printf("Enter string(s) to be disemvoweled: ");
+            FILE *inputFile = fread(buffer, sizeof(char), BUF_SIZE, stdin);
+            FILE *outputFile = fwrite(buffer, sizeof(char), BUF_SIZE, stdout);
+            break;
+        case 2:
+            FILE *inputFile = fread(buffer, sizeof(char), BUF_SIZE, fopen(argv[1], "r"));
+            FILE *outputFile = fwrite(buffer, sizeof(char), BUF_SIZE, stdout);
+            break;
+        case 3:
+            FILE *inputFile = fread(buffer, sizeof(char), BUF_SIZE, fopen(argv[1], "r"));
+            FILE *outputFile = fwrite(buffer, sizeof(char), BUF_SIZE, fopen(argv[2], "w"));
+            break;
+    }
+    
      if ( == NULL)
     {
-        return 0
+        //process a string
     }
-    return 1
+    //process as file
 }
 
 
 int main(int argc, char *argv[]) {
+    FILE *inputFile = stdin;
+    FILE *outputFile = stdout;
+    char buffer[BUF_SIZE];
     switch(argc) {
-        case 0:
-            FILE *inputFile = stdin;
-            FILE *outputFile = stdout;
-            break;
-        case 1:
-            FILE *inputFile = fopen(argv[0], "r");
-            FILE *outputFile = stout;
-            break;
         case 2:
-            FILE *inputFile = fopen(argv[0], "r");
-            FILE *outputFile = fopen(argv[1], "w");
+            FILE inputFile = fopen(argv[1], "r");
             break;
-        default:
-            FILE *inputFile = stdin;
-            FILE *outputFile = stdout;
+        case 3:
+            FILE inputFile = fopen(argv[1], "r");
+            FILE outputFile = fopen(argv[2], "w");
             break;
     }
-        // This sets these to `stdin` and `stdout` by default.
-        // You then need to set them to user specified files when the user
-        // provides files names as command line arguments.
-        
-
-    // Code that processes the command line arguments
-    // and sets up inputFile and outputFile.
 
     disemvowel(inputFile, outputFile);
 
