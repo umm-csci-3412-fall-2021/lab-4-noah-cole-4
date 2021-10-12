9ddfa0ae97788505dbdc7a3e88864a8859a56152
zeus-ctrl <noah.constable@gmail.com>
2021-10-12 11:34:56 -0500

    summarize_tree_ftw

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -5,56 +5,22 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <ftw.h>
 
+#define MAX_FTW_DEPTH 16
 static int num_dirs, num_regular;
 
-bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat() in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
-}
 
-/* 
- * I needed this because the multiple recursion means there's no way to
- * order them so that the definitions all precede the cause.
- */
-void process_path(const char*);
-
-void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
-}
-
-void process_file(const char* path) {
-  /*
-   * Update the number of regular files.
-   * This is as simple as it seems. :-)
-   */
-}
-
-void process_path(const char* path) {
-  if (is_dir(path)) {
-    process_directory(path);
-  } else {
-    process_file(path);
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+  if (typeflag == FTW_D) {
+    num_dirs++;
+  } else if (typeflag == FTW_F) {
+    num_regular++;
   }
+  return 0;
 }
 
-int main (int argc, char *argv[]) {
-  // Ensure an argument was provided.
+int main(int argc, char** argv) {
   if (argc != 2) {
     printf ("Usage: %s <path>\n", argv[0]);
     printf ("       where <path> is the file or root of the tree you want to summarize.\n");
@@ -64,7 +30,7 @@ int main (int argc, char *argv[]) {
   num_dirs = 0;
   num_regular = 0;
 
-  process_path(argv[1]);
+  ftw(argv[1], callback, MAX_FTW_DEPTH);
 
   printf("There were %d directories.\n", num_dirs);
   printf("There were %d regular files.\n", num_regular);
